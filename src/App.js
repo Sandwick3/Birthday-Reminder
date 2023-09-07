0cc2ad5b9324b81f775e362c15ad45560c740be8
Lloyd Clarete <lloyd.clarete@usjr.edu.ph>
2023-09-07 19:38:18 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [friendsData, setFriendsData] = useState([
+    {
+      name: 'Ying Won',
+      age: 29, 
+      imageSrc: "./img/a1-min.jpg",
+    },
+    {
+      name: 'Lil Sporty',
+      age: 32,
+      imageSrc: "./img/3-min.jpg",
+    },
+    {
+      name: 'Glannie Mae',
+      age: 20,
+      imageSrc: "./img/2-min.jpg",
+    },
+    {
+      name: 'Gwen Stacy',
+      age: 18,
+      imageSrc: "./img/5-min.jpg",
+    },
+    {
+      name: 'Lee Ji-eun',
+      age: 30,
+      imageSrc: "./img/IU.jpg",
+    }
+  ]);
+
+  // Function to clear the friendsData
+  const clearFriendsData = () => {
+    setFriendsData([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 style={{ fontSize: '15px' }}>24 birthdays today</h1>
+
+        <div className="container-alt">
+          <div className="friends-list">
+            {friendsData.map((friend, index) => (
+              <div key={index} className="friend">
+                <div className="profile-picture">
+                  <img src={friend.imageSrc} alt={friend.name} />
+                </div>
+                <div className="friend-details">
+                  <p className="friend-name">{friend.name}</p>
+                  <p className="friend-age" style={{ fontSize: '9px' }}>{friend.age} years old</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className="bottom-button" onClick={clearFriendsData}>Clear All</button>
+      </div>
     </div>
   );
 }
